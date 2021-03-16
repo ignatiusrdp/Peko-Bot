707b6ed5b542b63c17683abfccc211038b94943e
ignatius <ignatiusrahardi@gmail.com>
2021-03-16 17:42:59 +0700

    fixed pekofy command and add pain command

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { Client } = require('discord.js');
+const { Client, MessageAttachment  } = require('discord.js');
 const client = new Client();
 
 const COMMAND_PREFIX = "!"
@@ -15,35 +15,55 @@ client.on('ready' , () =>{
 
 client.on('message', (message)=>{
     if(message.author.bot) return;
+
     if(message.content.startsWith(COMMAND_PREFIX)){
         const [command, ...args] = message.content
         .substring(COMMAND_PREFIX.length)
         .split(/\s+/)
+
         switch (command) {
-            case "pekofy":
+            case "pekofy":  
                 if(message.reference !== null){
                     message.channel.messages.fetch(message.reference.messageID)
                         .then((repliedMsg)=> {
-                            if(repliedMsg.content.toLowerCase().endsWith("peko")){
-                                message.channel.send(`alredy pekofied peko`)
-                            } else {    
-                                message.channel.send(`${repliedMsg.content} peko`)
+                            var pekofyMsg;
+                            if (!repliedMsg.content.match(/\.\s/g)){
+                                pekofyMsg = (repliedMsg.content
+                                    .split(/\s+/)
+                                    .join(" ") + " ")
+                                    .replace(/\.\s/g, " **peko.** ");
+                            } else {
+                                pekofyMsg = repliedMsg.content.replace(/\.\s/g, " **peko.** ");
+                            } 
+                            
+                            if(pekofyMsg.toLowerCase().endsWith(" **peko.** ")) {
+                                message.channel.send(`${pekofyMsg}`)
+                            } else {
+                                message.channel.send(`${pekofyMsg} **peko.** `)
                             }
                         })
                         .catch(()=>{
                             message.channel.send("Pain peko, there is something wrong with the message peko")
                         });
                 } else {
-                    const pekofyArgs = args.join(" ");
-                    if(pekofyArgs.toLowerCase().endsWith("peko")){
-                        message.channel.send(`alredy pekofied peko`)
+                    var pekofyArgs;
+                    if(args.length == 1){
+                        pekofyArgs = (args.join(" ") + " ").replace(/\.\s/g, " **peko.** ");
                     } else {
-                        message.channel.send(`${pekofyArgs} peko`)
+                        pekofyArgs = args.join(" ").replace(/\.\s/g, " **peko.** ");
+                    }
+                    if(pekofyArgs.toLowerCase().endsWith(" **peko.** ")) {
+                        message.channel.send(`${pekofyArgs}`)
+                    } else {
+                        message.channel.send(`${pekofyArgs} **peko.** `)
                     }
                     
                 }
                 break;
-        
+            case "pain":
+                const attachment = new MessageAttachment("https://imgur.com/Pq89pHr.jpg");
+                message.channel.send("**Pain Peko**", attachment);
+                break;
             default:
                 message.channel.send("Pain peko, unknown command peko");
                 break;
